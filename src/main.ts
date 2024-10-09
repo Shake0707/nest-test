5ac6a0cc3de02270d6dd1ca4d6f0f1fe310218d8
shake0707 <shaxriyorburiyev7@gmail.com>
2024-10-08 16:42:35 -1200

    adding port var to env file and deactivated microservice

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,23 @@ import { ValidationPipe } from '@nestjs/common';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
+  const PORT = process.env.PORT;
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("api");
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4200);
-  console.log("HTTP app is listening on port 4200");
+  await app.listen(PORT);
+  console.log("HTTP app is listening on port " + PORT);
 
-  const microserviceApp = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
-    transport: Transport.TCP,
-    options: {
-      host: "localhost",
-      port: 8877
-    }
-  });
+  // const microserviceApp = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+  //   transport: Transport.TCP,
+  //   options: {
+  //     host: "localhost",
+  //     port: 8877
+  //   }
+  // });
 
-  await microserviceApp.listen();
-  console.log("Microservice is listening on port 8877");
+  // await microserviceApp.listen();
+  // console.log("Microservice is listening on port 8877");
 }
 
 bootstrap();
